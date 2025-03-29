712ef67ee69e64b76d3ee7944ef7f482d736f698
Sankalp Boolchandani <boolchandanisankalp@gmail.com>
2025-03-30 00:01:21 +0530

    adding lombok to project, removing boilerplate code

--- src/main/java/com/company/journalApp/entity/JournalEntry.java
+++ src/main/java/com/company/journalApp/entity/JournalEntry.java
@@ -1,13 +1,18 @@
 package com.company.journalApp.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
-import java.util.Date;
 
 @Document(collection = "journal_entries")
+@Data
+@NoArgsConstructor
+@ToString
 public class JournalEntry {
 
     @Id
@@ -15,36 +20,4 @@ public class JournalEntry {
     private String name;
     private String content;
     private LocalDateTime date;
-
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 }

92731097afbc57d053dc2fbb389998232b61639a
Sankalp Boolchandani <boolchandanisankalp@gmail.com>
2025-04-10 02:03:02 +0530

    overloading sendEmail method for future uses

--- src/main/java/com/company/journalApp/service/EmailService.java
+++ src/main/java/com/company/journalApp/service/EmailService.java
@@ -13,12 +13,28 @@ public class EmailService {
     @Autowired
     private JavaMailSender javaMailSender;
 
+    // demo test method for testing out email sending
     public void sendMail(){
         try {
             SimpleMailMessage msg=new SimpleMailMessage();
             msg.setTo("boolchandanisankalp@gmail.com");
             msg.setSubject("Mail test 2");
             msg.setText("it worked the third time, yml had indentation error nothing big! xd");
+            javaMailSender.send(msg);
+        } catch (Exception e) {
+            log.error("Exception while sending email ", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    // method overloading - main method to send email from outside the class
+    public void sendMail(String to, String subject, String message){
+        try {
+            SimpleMailMessage msg=new SimpleMailMessage();
+            msg.setTo(to);
+            msg.setSubject(subject);
+            msg.setText(message);
             javaMailSender.send(msg);
         } catch (Exception e) {
             log.error("Exception while sending email ", e);
